a05b19a989b9b71e1a91d45fb37631637a44ec1d
Philipp Marek <philipp@marek.priv.at>
2021-03-02 08:20:44 +0100

    Allow WITH-EXISTING-DOCUMENT to reuse already loaded data. WIP
    Need to find out whether the original in memory gets changed and
    possible corrupted on simultaneous accesses.

--- pdf-parser.lisp
+++ pdf-parser.lisp
@@ -539,13 +539,17 @@ Returns the first unused object-number."
 (export 'remove-page)
 
 (defmacro with-existing-document ((file &key (creator "") author title subject keywords) &body body)
-  `(let* ((*document* (read-pdf-file ,file))
-	  (*root-page* (root-page *document*))
-	  (*page-number* 0)
-          (*name-counter* 100))
-     (add-doc-info *document* :creator ,creator :author ,author
-		   :title ,title :subject ,subject :keywords ,keywords)
-    ,@body))
+  (let ((file-sym (gensym "FILE-")))
+    `(let* ((,file-sym ,file)
+            (*document* (if (typep ,file-sym 'document)
+                            (copy-pdf-structure ,file-sym)
+                            (read-pdf-file ,file-sym)))
+            (*root-page* (root-page *document*))
+            (*page-number* 0)
+            (*name-counter* 100))
+       (add-doc-info *document* :creator ,creator :author ,author
+                     :title ,title :subject ,subject :keywords ,keywords)
+       ,@body)))
 
 (export 'with-existing-document)
 
